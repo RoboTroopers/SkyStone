1b6af67c09ffb4c6d4cfce707186bdf9759cc16e
ripWaterSheep <joshuadherer@icloud.com>
2019-10-29 18:27:32 -0400

    Cleaned up driveConstants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveConstants.java
@@ -1,32 +1,37 @@
 package org.firstinspires.ftc.teamcode;
 
-public class DriveConstants {
 
-    /*
+public class DriveConstants {
+    
+    /**
      *
      * All the robot's constants relating to its motion and measurements
-     *
+     * Every linear measurement uses inches.
+     * 
      */
-
+    
     public static final double WHEEL_RADIUS = 2;
     public static final double WHEEL_CIRCUMFERENCE = WHEEL_RADIUS*2*Math.PI;
-
+    
     // length between front & back wheels
     public static final double WHEEL_BASE = 10;
-
+    
     // width between left and right wheels
     public static final double TRACK_WIDTH = 16;
-
+    
+    
     /*
-     * diameter of the circumcircle of the robot, really the distance between centers of 2 diagonal
-     * wheels (hypotenuse of wheelbase and track width)
+     * CIRCUMCIRCLE: Circle circumscribed about points of a polygon.
+     *
+     * CIRCUMDIAMETER: Diameter of the circumcircle of the robot, really the distance between ground contact points
+     * of 2 diagonal wheels (hypotenuse of wheelbase and track width)
+     * 
      */
+    
     public static final double CIRCUMDIAMETER = Math.hypot(WHEEL_BASE, TRACK_WIDTH);
-    //Circumference of circle
     public static final double CIRCUMCIRCLE_CIRCUMFERENCE = CIRCUMDIAMETER*Math.PI;
     
-
-
-
-
+    public static final double WEBCAM_OFFSET = 5;
+    
+    
 }
